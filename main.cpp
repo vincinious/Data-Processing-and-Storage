7baccd79d9fd086fbb9028c465210a95d4911fe8
vincinious <87542036+vincinious@users.noreply.github.com>
2024-12-12 10:54:10 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "InMemoryDB.h"
+#include "inMemoryDB.h"
 #include <iostream>
 
 using namespace std;
@@ -9,16 +9,20 @@ int main()
 
     try 
     {
+        /* Try to get value of key A from the main db */
         cout << "Initial get(\"A\"): " << (db.get("A").has_value() ? to_string(db.get("A").value()) : "null") << '\n';
 
+        /* Start a new transaction, add A, 5 as key and value, and attempt to get the value in the transaction */
         db.begin_transaction();
         db.put("A", 5);
         cout << "After put(\"A\", 5), get(\"A\"): " << (db.get("A").has_value() ? to_string(db.get("A").value()) : "null") << '\n';
 
+        /* Update the key to A, 6 within the transaction*/ 
         db.put("A", 6);
         db.commit();
         cout << "After commit(), get(\"A\"): " << (db.get("A").has_value() ? to_string(db.get("A").value()) : "null") << '\n';
 
+        /* Start a new transaction, add A, 6 as key and value, and attempt to get the value in the transaction */
         db.begin_transaction();
         db.put("B", 10);
         db.rollback();
@@ -26,6 +30,7 @@ int main()
 
     } 
     catch (const exception& e)
+    /* Display any errors */
     { cerr << "Exception: " << e.what() << '\n'; }
 
     return 0;
