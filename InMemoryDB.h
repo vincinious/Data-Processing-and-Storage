354b7699e60d7dc273a05eaf3c00ae7ed370145b
vincinious <87542036+vincinious@users.noreply.github.com>
2024-12-12 10:46:09 -0800

    Included InMemoryDB.h

---
+++ InMemoryDB.h
@@ -0,0 +1,24 @@
+#include <string>
+#include <unordered_map>
+#include <stack>
+#include <stdexcept>
+
+using namespace std;
+
+class InMemoryDB 
+{
+private:
+    unordered_map<string, int> database;
+    unordered_map<string, int> transactionData;
+    stack<unordered_map<string, int>> transactionStack;
+    bool transactionActive = false;
+
+public:
+    int get(const string& key);              /* return value associated with the key or null if doesnt exist */
+    void put(const string& key, int val);    /* create a new key if it doesnt exist, or update the existing key value */
+    void begin_transaction();                /* start a new transaction */
+    void commit();                           /* applies changes within the transaction to the main state */
+    void rollback();                         /* abort all changes within the transaction and reset */
+};
+
+
